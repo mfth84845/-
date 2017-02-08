dc0d9ad73d22b02abfd261f2659d6a55082475d9
mfth84845 <1461546139@qq.com>
2017-02-08 10:57:28 +0800

    Add files via upload

---
+++ PlayAnimationOperation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimationOperation : Operation {
+	public Animator anim;
+	public string state;
+
+	public override void Execute ()
+	{
+		anim.Play (state);	
+	}
+
+	public override bool IsOver {
+		get 
+		{
+			return true;
+		}
+	}
+}
